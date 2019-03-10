31ed6b8c9e20b720c155d37523d87cf8e0b6d2bb
dragon-warrior-nyc <46853674+dragon-warrior-nyc@users.noreply.github.com>
2019-03-09 20:38:16 -0500

    Update sys_argv.py

--- sys_argv.py
+++ sys_argv.py
@@ -1,9 +1,11 @@
 import sys
-if len(sys.argv) > 1:
-    print( "~ Script: " + sys.argv[0])
-    print( "~ Arg : " + sys.argv[1])
-else:
-    print(" No arguments ")
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print( "~ Script: " + sys.argv[0])
+        print( "~ Arg : " + sys.argv[1])
+    else:
+        print(" No arguments ")
 
 # try the following in command line
 # python sys_argv.py
